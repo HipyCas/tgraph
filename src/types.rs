fc26b77771369c5591b46a5559f2311ed69c8dd0
Hipy Cas <hipycas@gmail.com>
2022-04-30 18:50:25 +0200

    Document Character struct
    Adds a fairly simple documentation to the Character struct, all its
    variants, methods and trait implementations.

--- src/types.rs
+++ src/types.rs
@@ -3,32 +3,47 @@ use derivative::Derivative;
 use std::fmt;
 use std::fmt::Write;
 
+/// Enum used in `tgraph` to represent painting character.
 #[derive(Copy, Clone, Derivative, Debug)]
 #[derivative(Default)]
 pub enum Character {
+  /// Unicode circle with cross character (¤)
   CrossCircle,
+  /// Copyright character (®)
+  Copyright,
+  /// Backwards compatibility with `Character::Copyright`
   Registered,
+  /// Unicode times/multiplication symbol (×)
   Times,
+  /// Unicode Cyrillic Zhe letter character (ж)
   CyrillicZhe,
-  ///   ҉
+  /// Unicode Cyrillic millions character (    ҉ )
   CyrillicMillions,
-  ///   ҈        
+  /// Unicode Cyrillic hundred thousands character (    ҈ )        
   CyrillicHundredThousands,
-  Bullet,        // •
-  EmptyBullet,   // ∘
-  SmallTriangle, // ‣
-  CrossPoint,    // ※
-  Asterisk,      // ∗
+  /// Unicode bullet character (•)
+  Bullet,
+  /// Unicode empty bullet character (∘)
+  EmptyBullet,
+  /// Unicode small right-pointing triangle (‣)
+  SmallTriangle,
+  /// Unicode cross with points character (※)
+  CrossPoint,
+  /// ASCII and Unicode asterisk character (∗)
+  Asterisk,
   #[derivative(Default)]
-  Star, // ⁕
+  /// **(Default)** Unicode star character (⁕)
+  Star,
+  /// Tuple wrapper for rust `char` type, allows to use
   Custom(char),
 }
 
 impl Character {
+  /// Transforms the `Character` variant to the corresponding Rust `char` character. For the `Character::Custom(char)` variant, it returns the `char` that the variant wraps.
   pub fn as_char(&self) -> char {
     match self {
       Character::CrossCircle => '¤',
-      Character::Registered => '®',
+      Character::Copyright | Character::Registered => '®',
       Character::Times => '×',
       Character::CyrillicZhe => 'ж',
       Character::CyrillicMillions => '\u{0489}',
@@ -45,18 +60,21 @@ impl Character {
 }
 
 impl From<char> for Character {
+  /// Wraps the `char` in a `Character::Custom(char)` variant.
   fn from(c: char) -> Character {
     Character::Custom(c)
   }
 }
 
 impl Into<char> for Character {
+  /// Calls `Character::as_char(&self)`
   fn into(self) -> char {
     self.as_char()
   }
 }
 
 impl fmt::Display for Character {
+  /// Prints the character obtained from calling `Character::as_char()` on `self`
   fn fmt(&self, f: &mut fmt::Formatter<'_>) -> Result<(), fmt::Error> {
     f.write_char(self.as_char())
   }
