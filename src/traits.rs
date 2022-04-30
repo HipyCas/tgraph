4a39ba97a213f3ad4da06d71aeff69d4cd39c710
Vincent Foulon <vincent.foulon80@gmail.com>
2022-04-30 23:41:55 +0200

    Fixed warnings + ran cargo fmt

--- src/traits.rs
+++ src/traits.rs
@@ -1,19 +1,20 @@
 pub trait AsF64 {
-  fn as_f64(self) -> f64;
-  fn from_f64(v: f64) -> Self;
+    #[allow(clippy::wrong_self_convention)]
+    fn as_f64(self) -> f64;
+    fn from_f64(v: f64) -> Self;
 }
 
 macro_rules! impl_asf64 {
-  ($type:ty) => {
-    impl AsF64 for $type {
-      fn as_f64(self) -> f64 {
-        self as f64
-      }
-      fn from_f64(v: f64) -> $type {
-        v.round() as $type
-      }
-    }
-  };
+    ($type:ty) => {
+        impl AsF64 for $type {
+            fn as_f64(self) -> f64 {
+                self as f64
+            }
+            fn from_f64(v: f64) -> $type {
+                v.round() as $type
+            }
+        }
+    };
 }
 
 impl_asf64!(u8);
@@ -28,43 +29,43 @@ impl_asf64!(i64);
 impl_asf64!(i128);
 
 impl AsF64 for f32 {
-  fn as_f64(self) -> f64 {
-    self as f64
-  }
+    fn as_f64(self) -> f64 {
+        self as f64
+    }
 
-  fn from_f64(v: f64) -> f32 {
-    v as f32
-  }
+    fn from_f64(v: f64) -> f32 {
+        v as f32
+    }
 }
 
 impl AsF64 for f64 {
-  fn as_f64(self) -> f64 {
-    self
-  }
+    fn as_f64(self) -> f64 {
+        self
+    }
 
-  fn from_f64(v: f64) -> f64 {
-    v
-  }
+    fn from_f64(v: f64) -> f64 {
+        v
+    }
 }
 
 pub trait MaybeAsF64 {
-  fn maybe_as_f64(self) -> Option<f64>;
+    fn maybe_as_f64(self) -> Option<f64>;
 }
 
 impl<T> MaybeAsF64 for T
 where
-  T: AsF64,
+    T: AsF64,
 {
-  fn maybe_as_f64(self) -> Option<f64> {
-    Some(self.as_f64())
-  }
+    fn maybe_as_f64(self) -> Option<f64> {
+        Some(self.as_f64())
+    }
 }
 
 impl<T> MaybeAsF64 for Option<T>
 where
-  T: AsF64,
+    T: AsF64,
 {
-  fn maybe_as_f64(self) -> Option<f64> {
-    self.map(|v| v.as_f64())
-  }
+    fn maybe_as_f64(self) -> Option<f64> {
+        self.map(|v| v.as_f64())
+    }
 }
--- src/types.rs
+++ src/types.rs
@@ -7,77 +7,77 @@ use std::fmt::Write;
 #[derive(Copy, Clone, Derivative, Debug)]
 #[derivative(Default)]
 pub enum Character {
-  /// Unicode circle with cross character (¤)
-  CrossCircle,
-  /// Copyright character (®)
-  Copyright,
-  /// Backwards compatibility with `Character::Copyright`
-  Registered,
-  /// Unicode times/multiplication symbol (×)
-  Times,
-  /// Unicode Cyrillic Zhe letter character (ж)
-  CyrillicZhe,
-  /// Unicode Cyrillic millions character (    ҉ )
-  CyrillicMillions,
-  /// Unicode Cyrillic hundred thousands character (    ҈ )        
-  CyrillicHundredThousands,
-  /// Unicode bullet character (•)
-  Bullet,
-  /// Unicode empty bullet character (∘)
-  EmptyBullet,
-  /// Unicode small right-pointing triangle (‣)
-  SmallTriangle,
-  /// Unicode cross with points character (※)
-  CrossPoint,
-  /// ASCII and Unicode asterisk character (∗)
-  Asterisk,
-  #[derivative(Default)]
-  /// **(Default)** Unicode star character (⁕)
-  Star,
-  /// Tuple wrapper for rust `char` type, allows to use
-  Custom(char),
+    /// Unicode circle with cross character (¤)
+    CrossCircle,
+    /// Copyright character (®)
+    Copyright,
+    /// Backwards compatibility with `Character::Copyright`
+    Registered,
+    /// Unicode times/multiplication symbol (×)
+    Times,
+    /// Unicode Cyrillic Zhe letter character (ж)
+    CyrillicZhe,
+    /// Unicode Cyrillic millions character (    ҉ )
+    CyrillicMillions,
+    /// Unicode Cyrillic hundred thousands character (    ҈ )        
+    CyrillicHundredThousands,
+    /// Unicode bullet character (•)
+    Bullet,
+    /// Unicode empty bullet character (∘)
+    EmptyBullet,
+    /// Unicode small right-pointing triangle (‣)
+    SmallTriangle,
+    /// Unicode cross with points character (※)
+    CrossPoint,
+    /// ASCII and Unicode asterisk character (∗)
+    Asterisk,
+    #[derivative(Default)]
+    /// **(Default)** Unicode star character (⁕)
+    Star,
+    /// Tuple wrapper for rust `char` type, allows to use
+    Custom(char),
 }
 
 impl Character {
-  /// Transforms the `Character` variant to the corresponding Rust `char` character. For the `Character::Custom(char)` variant, it returns the `char` that the variant wraps.
-  pub fn as_char(&self) -> char {
-    match self {
-      Character::CrossCircle => '¤',
-      Character::Copyright | Character::Registered => '®',
-      Character::Times => '×',
-      Character::CyrillicZhe => 'ж',
-      Character::CyrillicMillions => '\u{0489}',
-      Character::CyrillicHundredThousands => '\u{0488}',
-      Character::Bullet => '\u{2022}',
-      Character::EmptyBullet => '\u{2218}',
-      Character::SmallTriangle => '\u{2023}',
-      Character::CrossPoint => '\u{203B}',
-      Character::Asterisk => '\u{2217}',
-      Character::Star => '\u{2055}',
-      Character::Custom(c) => *c,
+    /// Transforms the `Character` variant to the corresponding Rust `char` character. For the `Character::Custom(char)` variant, it returns the `char` that the variant wraps.
+    pub fn as_char(&self) -> char {
+        match self {
+            Character::CrossCircle => '¤',
+            Character::Copyright | Character::Registered => '®',
+            Character::Times => '×',
+            Character::CyrillicZhe => 'ж',
+            Character::CyrillicMillions => '\u{0489}',
+            Character::CyrillicHundredThousands => '\u{0488}',
+            Character::Bullet => '\u{2022}',
+            Character::EmptyBullet => '\u{2218}',
+            Character::SmallTriangle => '\u{2023}',
+            Character::CrossPoint => '\u{203B}',
+            Character::Asterisk => '\u{2217}',
+            Character::Star => '\u{2055}',
+            Character::Custom(c) => *c,
+        }
     }
-  }
 }
 
 impl From<char> for Character {
-  /// Wraps the `char` in a `Character::Custom(char)` variant.
-  fn from(c: char) -> Character {
-    Character::Custom(c)
-  }
+    /// Wraps the `char` in a `Character::Custom(char)` variant.
+    fn from(c: char) -> Character {
+        Character::Custom(c)
+    }
 }
 
-impl Into<char> for Character {
-  /// Calls `Character::as_char(&self)`
-  fn into(self) -> char {
-    self.as_char()
-  }
+impl From<Character> for char {
+    /// Calls `Character::as_char(&self)`
+    fn from(val: Character) -> Self {
+        val.as_char()
+    }
 }
 
 impl fmt::Display for Character {
-  /// Prints the character obtained from calling `Character::as_char()` on `self`
-  fn fmt(&self, f: &mut fmt::Formatter<'_>) -> Result<(), fmt::Error> {
-    f.write_char(self.as_char())
-  }
+    /// Prints the character obtained from calling `Character::as_char()` on `self`
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> Result<(), fmt::Error> {
+        f.write_char(self.as_char())
+    }
 }
 
 #[derive(Derivative, Copy, Clone)]
@@ -85,43 +85,43 @@ impl fmt::Display for Character {
 pub struct ColorWrapper(Color);
 
 impl Default for ColorWrapper {
-  fn default() -> ColorWrapper {
-    ColorWrapper(Color::Blue)
-  }
+    fn default() -> ColorWrapper {
+        ColorWrapper(Color::Blue)
+    }
 }
 
 impl From<Color> for ColorWrapper {
-  fn from(c: Color) -> ColorWrapper {
-    ColorWrapper(c)
-  }
+    fn from(c: Color) -> ColorWrapper {
+        ColorWrapper(c)
+    }
 }
 
-impl Into<Color> for ColorWrapper {
-  fn into(self) -> Color {
-    self.0
-  }
+impl From<ColorWrapper> for Color {
+    fn from(val: ColorWrapper) -> Self {
+        val.0
+    }
 }
 
 #[derive(Derivative)]
 #[derivative(Default, Debug)]
 pub struct Scales {
-  #[derivative(Default(value = "1f64"))]
-  pub x: f64,
-  #[derivative(Default(value = "1f64"))]
-  pub y: f64,
+    #[derivative(Default(value = "1f64"))]
+    pub x: f64,
+    #[derivative(Default(value = "1f64"))]
+    pub y: f64,
 }
 
 impl From<(f64, f64)> for Scales {
-  fn from((x, y): (f64, f64)) -> Scales {
-    Scales { x, y }
-  }
+    fn from((x, y): (f64, f64)) -> Scales {
+        Scales { x, y }
+    }
 }
 
 impl From<(Option<f64>, Option<f64>)> for Scales {
-  fn from((maybe_x, maybe_y): (Option<f64>, Option<f64>)) -> Scales {
-    Scales {
-      x: maybe_x.unwrap_or(1f64),
-      y: maybe_y.unwrap_or(1f64),
+    fn from((maybe_x, maybe_y): (Option<f64>, Option<f64>)) -> Scales {
+        Scales {
+            x: maybe_x.unwrap_or(1f64),
+            y: maybe_y.unwrap_or(1f64),
+        }
     }
-  }
 }
